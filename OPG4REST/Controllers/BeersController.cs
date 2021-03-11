f458530254b52b536cc85fb70c956cfec286f1eb
Gleb Olsen <gleb.olsen@gmail.com>
2021-03-11 17:26:17 +0100

    Add project files.

---
+++ OPG4REST/Controllers/BeersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OBG1;
+using OPG4REST.Managers;
+
+namespace OPG4REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeersController : ControllerBase
+    {
+        private readonly BeersManager _manager = new BeersManager();
+        // GET: api/<BeersController>
+        [HttpGet]
+        public IEnumerable<Beer> Get()
+        {
+            return _manager.Getall();
+        }
+
+        // GET api/<BeersController>/5
+        [HttpGet("{id}")]
+        public Beer Get(int id)
+        {
+            return _manager.GetId(id);
+        }
+
+        // POST api/<BeersController>
+        [HttpPost]
+        public void Post([FromBody] Beer value)
+        {
+            _manager.Add(value);
+        }
+
+        // PUT api/<BeersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Beer value)
+        {
+            _manager.Update(id, value);
+        }
+
+        // DELETE api/<BeersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _manager.Delete(id);
+        }
+    }
+}
---
+++ OPG4REST/Managers/BeersManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OBG1;
+
+namespace OPG4REST.Managers
+{
+    public class BeersManager
+    {
+        private static int _nextId = 1;
+        private static List<Beer> _beers = new List<Beer>()
+        {
+            new Beer("Beer1",_nextId++,10.95,5),
+            new Beer("Beer2",_nextId++,11.95,6)
+        };
+
+        public IEnumerable<Beer> Getall()
+        {
+            return _beers;
+        }
+
+        public Beer GetId(int id)
+        {
+            return _beers.Find(beer => beer.ID == id);
+        }
+
+        public void Add(Beer value)
+        {
+            value.ID = _nextId++;
+            _beers.Add(value);
+        }
+
+        public void Update(int id, Beer value)
+        {
+            Beer beer = _beers.Find(beer1 => beer1.ID == id);
+            if (beer == null) return;
+            beer.Name = value.Name;
+            beer.Price = value.Price;
+            beer.Adv = value.Adv;
+        }
+
+        public void Delete(int id)
+        {
+            Beer beer = _beers.Find(beer1 => beer1.ID == id);
+            if (beer == null) return;
+            _beers.Remove(beer);
+        }
+    }
+}
